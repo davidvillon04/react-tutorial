5880ea35a64aee3b35f2d02ac40983a16d68c163
davidvillon04 <davidv04@vt.edu>
2025-04-02 20:10:20 -0400

    Created Game function

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
    );
 }
 
-export default function Board() {
+function Board() {
    const [xIsNext, setXIsNext] = useState(true);
    const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -74,4 +74,20 @@ function calculateWinner(squares) {
       }
    }
    return null;
+}
+
+export default function Game() {
+   const [xIsNext, setXIsNext] = useState(true);
+   const [history, setHistory] = useState([Array(9).fill(null)]);
+
+   return (
+      <div className="game">
+         <div className="game-board">
+            <Board />
+         </div>
+         <div className="game-info">
+            <ol>{/*TODO*/}</ol>
+         </div>
+      </div>
+   );
 }
